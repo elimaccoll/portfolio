4b23cca25be4292ac6ffda5c9e9c1936940be679
Eli MacColl <elimaccoll@gmail.com>
2023-10-11 17:10:48 -0400

    added mouse trail

---
+++ scripts/trail.js
@@ -0,0 +1,17 @@
+function trail(e) {
+  var div = document.createElement("div");
+
+  div.classList.add("mouse-trail");
+
+  Object.assign(div.style, {
+    top: e.pageY - 10 + "px",
+    left: e.pageX - 10 + "px",
+  });
+
+  document.body.appendChild(div);
+  setTimeout(function () {
+    document.body.removeChild(div);
+  }, 500);
+}
+
+addEventListener("mousemove", trail);
