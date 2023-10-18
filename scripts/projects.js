a0a5ac0a45fc1a34b77adda5289ce83f28a0553f
Eli MacColl <elimaccoll@gmail.com>
2023-10-17 20:30:32 -0400

    added two more projects and worked on responsive styling

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,24 +1,81 @@
-document.getElementById("project1-small").addEventListener("mouseover", () => {
-  document.getElementById("project1-small").src = "assets/terrarium-small.gif";
-});
-document.getElementById("project1-small").addEventListener("mouseleave", () => {
-  document.getElementById("project1-small").src = "assets/terrarium-small.png";
-});
-
-document.getElementById("project1-medium").addEventListener("mouseover", () => {
-  document.getElementById("project1-medium").src =
-    "assets/terrarium-medium.gif";
+// Terrarium
+document.getElementById("terrarium-small").addEventListener("mouseover", () => {
+  document.getElementById("terrarium-small").src = "assets/terrarium-small.gif";
 });
 document
-  .getElementById("project1-medium")
+  .getElementById("terrarium-small")
   .addEventListener("mouseleave", () => {
-    document.getElementById("project1-medium").src =
+    document.getElementById("terrarium-small").src =
+      "assets/terrarium-small.png";
+  });
+
+document
+  .getElementById("terrarium-medium")
+  .addEventListener("mouseover", () => {
+    document.getElementById("terrarium-medium").src =
+      "assets/terrarium-medium.gif";
+  });
+document
+  .getElementById("terrarium-medium")
+  .addEventListener("mouseleave", () => {
+    document.getElementById("terrarium-medium").src =
       "assets/terrarium-medium.png";
   });
 
-document.getElementById("project1-large").addEventListener("mouseover", () => {
-  document.getElementById("project1-large").src = "assets/terrarium-large.gif";
+document.getElementById("terrarium-large").addEventListener("mouseover", () => {
+  document.getElementById("terrarium-large").src = "assets/terrarium-large.gif";
 });
-document.getElementById("project1-large").addEventListener("mouseleave", () => {
-  document.getElementById("project1-large").src = "assets/terrarium-large.png";
+document
+  .getElementById("terrarium-large")
+  .addEventListener("mouseleave", () => {
+    document.getElementById("terrarium-large").src =
+      "assets/terrarium-large.png";
+  });
+
+// ReviewSpot
+document
+  .getElementById("reviewspot-small")
+  .addEventListener("mouseover", () => {
+    document.getElementById("reviewspot-small").src =
+      "assets/reviewspot-small.gif";
+  });
+document
+  .getElementById("reviewspot-small")
+  .addEventListener("mouseleave", () => {
+    document.getElementById("reviewspot-small").src =
+      "assets/reviewspot-small.png";
+  });
+
+document
+  .getElementById("reviewspot-medium")
+  .addEventListener("mouseover", () => {
+    document.getElementById("reviewspot-medium").src =
+      "assets/reviewspot-medium.gif";
+  });
+document
+  .getElementById("reviewspot-medium")
+  .addEventListener("mouseleave", () => {
+    document.getElementById("reviewspot-medium").src =
+      "assets/reviewspot-medium.png";
+  });
+
+document
+  .getElementById("reviewspot-large")
+  .addEventListener("mouseover", () => {
+    document.getElementById("reviewspot-large").src =
+      "assets/reviewspot-large.gif";
+  });
+document
+  .getElementById("reviewspot-large")
+  .addEventListener("mouseleave", () => {
+    document.getElementById("reviewspot-large").src =
+      "assets/reviewspot-large.png";
+  });
+
+// CV Embed Image
+document.getElementById("embed").addEventListener("mouseover", () => {
+  document.getElementById("embed").src = "assets/embed.gif";
+});
+document.getElementById("embed").addEventListener("mouseleave", () => {
+  document.getElementById("embed").src = "assets/embed.png";
 });
