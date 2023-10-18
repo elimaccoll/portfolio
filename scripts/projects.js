5497c5c28a5d335c8afc2ad3b6cde6df711085ff
Eli MacColl <elimaccoll@gmail.com>
2023-10-17 21:12:24 -0400

    changed about me image and trying to fix image file paths

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,35 +1,37 @@
 // Terrarium
 document.getElementById("terrarium-small").addEventListener("mouseover", () => {
-  document.getElementById("terrarium-small").src = "assets/terrarium-small.gif";
+  document.getElementById("terrarium-small").src =
+    "./assets/terrarium-small.gif";
 });
 document
   .getElementById("terrarium-small")
   .addEventListener("mouseleave", () => {
     document.getElementById("terrarium-small").src =
-      "assets/terrarium-small.png";
+      "./assets/terrarium-small.png";
   });
 
 document
   .getElementById("terrarium-medium")
   .addEventListener("mouseover", () => {
     document.getElementById("terrarium-medium").src =
-      "assets/terrarium-medium.gif";
+      "./assets/terrarium-medium.gif";
   });
 document
   .getElementById("terrarium-medium")
   .addEventListener("mouseleave", () => {
     document.getElementById("terrarium-medium").src =
-      "assets/terrarium-medium.png";
+      "./assets/terrarium-medium.png";
   });
 
 document.getElementById("terrarium-large").addEventListener("mouseover", () => {
-  document.getElementById("terrarium-large").src = "assets/terrarium-large.gif";
+  document.getElementById("terrarium-large").src =
+    "./assets/terrarium-large.gif";
 });
 document
   .getElementById("terrarium-large")
   .addEventListener("mouseleave", () => {
     document.getElementById("terrarium-large").src =
-      "assets/terrarium-large.png";
+      "./assets/terrarium-large.png";
   });
 
 // ReviewSpot
@@ -37,45 +39,45 @@ document
   .getElementById("reviewspot-small")
   .addEventListener("mouseover", () => {
     document.getElementById("reviewspot-small").src =
-      "assets/reviewspot-small.gif";
+      "./assets/reviewspot-small.gif";
   });
 document
   .getElementById("reviewspot-small")
   .addEventListener("mouseleave", () => {
     document.getElementById("reviewspot-small").src =
-      "assets/reviewspot-small.png";
+      "./assets/reviewspot-small.png";
   });
 
 document
   .getElementById("reviewspot-medium")
   .addEventListener("mouseover", () => {
     document.getElementById("reviewspot-medium").src =
-      "assets/reviewspot-medium.gif";
+      "./assets/reviewspot-medium.gif";
   });
 document
   .getElementById("reviewspot-medium")
   .addEventListener("mouseleave", () => {
     document.getElementById("reviewspot-medium").src =
-      "assets/reviewspot-medium.png";
+      "./assets/reviewspot-medium.png";
   });
 
 document
   .getElementById("reviewspot-large")
   .addEventListener("mouseover", () => {
     document.getElementById("reviewspot-large").src =
-      "assets/reviewspot-large.gif";
+      "./assets/reviewspot-large.gif";
   });
 document
   .getElementById("reviewspot-large")
   .addEventListener("mouseleave", () => {
     document.getElementById("reviewspot-large").src =
-      "assets/reviewspot-large.png";
+      "./assets/reviewspot-large.png";
   });
 
 // CV Embed Image
 document.getElementById("embed").addEventListener("mouseover", () => {
-  document.getElementById("embed").src = "assets/embed.gif";
+  document.getElementById("embed").src = "./assets/embed.gif";
 });
 document.getElementById("embed").addEventListener("mouseleave", () => {
-  document.getElementById("embed").src = "assets/embed.png";
+  document.getElementById("embed").src = "./assets/embed.png";
 });
